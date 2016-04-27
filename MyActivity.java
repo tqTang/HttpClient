71718b6622d0c23d22c47a94e99fbb488309aee8
tqTang <tqkamza@gmail.com>
2016-04-27 12:51:42 +0800

    Update MyActivity.java

--- MyActivity.java
+++ MyActivity.java
@@ -11,28 +11,15 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MyActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.example.administrator.myfirstapp.MESSAGE";
     TextView textView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        
         textView = (TextView)findViewById(R.id.textView);
         checkConnet();
-
-
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     /**
@@ -41,18 +28,18 @@ public class MyActivity extends AppCompatActivity {
     public void checkConnet() {
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        
         if (networkInfo != null && networkInfo.isConnected()) {
             //TODO attempting to perform data transactions.
             MyAsyncTask myAsyncTask = new MyAsyncTask() {
                 @Override
                 protected void onPostExecute(String s) {
                     super.onPostExecute(s);
-                    Log.d("Debug","post"+s);
+                   
                     textView.setText(s);
-
                 }
             };
-            myAsyncTask.execute("http://blog.csdn.net/lmj623565791/article/details/47911083");
+            myAsyncTask.execute("http://...");
 
         } else {
             // TODO display error
@@ -60,26 +47,4 @@ public class MyActivity extends AppCompatActivity {
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
